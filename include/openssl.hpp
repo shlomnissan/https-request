09803cd16711fb1054b534d2a85e0e1fac17c8dd
Shlomi Nissan <snissan@chanzuckerberg.com>
2023-04-18 13:26:22 -0700

    Remove empty OpenSSL class

--- include/openssl.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Betamark Pty Ltd. All rights reserved.
-// Author: Shlomi Nissan (shlomi@betamark.com)
-
-#pragma once
-
-namespace Net {
-  
-}
--- src/openssl.cc
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 Betamark Pty Ltd. All rights reserved.
-// Author: Shlomi Nissan (shlomi@betamark.com)
-
-#include "openssl.hpp"
-
-namespace Net {
-
-}
